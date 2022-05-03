509b6fa1e62eaf3590938ebaa7abd27de7e6df20
manupg3 <man_uh_14@hotmail.com>
2022-05-03 17:40:21 -0300

    Primer Commit

---
+++ argon-dashboard-master/ecommerce/cart-sidebar.php
@@ -0,0 +1,24 @@
+
+<link rel="stylesheet" href="css/sidebarcartcss.css">
+
+<div class="cart-sidebar">
+    
+<div class="title-sidebar">
+        <h3>Tu Pedido</h3>
+          
+   </div>
+<div class="cart-body">
+  <div class="cart-item">
+    <div class="img">
+        <img src="" alt="">
+    </div>
+     <div class="">
+       <h5>Producto 1</h5>
+       <h5>5000</h5>
+     </div> 
+     <div class="buttons-footer">
+         <button class="checkout-sidebar-cart">Finalizar Compra</button>
+     </div>
+  </div>
+</div>
+</div>
